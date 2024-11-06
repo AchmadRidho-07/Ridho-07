b6d7797562d361c5fbcd5570787ea8f070197494
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-06 20:27:24 +0700

    modified

--- love.py
+++ love.py
@@ -3,18 +3,18 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Perasaan tersulit adalah ketika hatimu
-    terikat pada seseorang.
-    
-    kamu sadar bahwa kamu tidak dapat memilikinya,
-    dan kamu pun tidak kuasa untuk menjauh darinya
+    Persaan tersulit adalah ketika hatimu
+    terikat oleh seseorang.
+
+    dan kamu sadar, kamu tak dapat memilikinya
+    dan kamu pun tidak kuasa untuk menjauh darinya.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
-        time.sleep(0.10) # penundaan 0.10 detik per huruf
+        time.sleep(0.10) # Penundaan 0.10 detik per huruf
 
 def main():
-    jumlah_ulang = 1 # set jumlah pengulangan
+    jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
 
             tampilkan_kalimat_kata_per_huruf()
