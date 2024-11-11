11a2edec559d446816be7ac17b74798203f9bb6d
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-11 20:06:33 +0700

    modified

--- love.py
+++ love.py
@@ -3,8 +3,8 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    SEPI itu bukan ketika semuanya meninggalkanmu,
-    tapi ketika ALLAH tidak lagi ada dihatimu.
+    Aku kira kamu adalah penyembuh dari semua luka yang ada,
+    namun faktanya luka itu semakin bertambah.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
@@ -14,9 +14,9 @@ def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
 
-            tampilkan_kalimat_kata_per_huruf()
-            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-            print('\n' + '-' * 40) # Pemisah antar pengulangan
+        tampilkan_kalimat_kata_per_huruf()
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+        print('\n' + '-' * 40) # Pemisah antar pengulangan
 
-# Memanggil fungsi main untuk mejalankan program
+# memanggil fungsi main untuk menjalakan program
 main()
