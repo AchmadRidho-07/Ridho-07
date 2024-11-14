f624641533ab93d7c97c5fdb4f357ebe5b9e580c
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-14 20:19:36 +0700

    modified

--- love.py
+++ love.py
@@ -3,21 +3,17 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Jangan pernah takut membuka hati hanya karena masa lalumu,
-    trauma boleh, tapi jangan abaikan orang yang mendekatimu,
-    hanya karena ketakutanmu dimasa lalu.
-
-    ingat, jangan sampai orang-orang yang kamu abaikan
-    itu adalah jawaban dari do'amu yang setiap malam kamu minta.
+    Kamu berhasil mengisi kosongku,
+    kamu berhasil menenangkan kerasku,
+    namun, kamu gagal menjadi milikku.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
-        time.sleep(0.05) # Penundaan 0.05 detik per huruf
+        time.sleep(0.10) # Penundaan 0.10 detik per huruf
 
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-
             tampilkan_kalimat_kata_per_huruf()
             time.sleep(1) # Penundaan 1 detik sebelum pengulangan
             print('\n' + '-' * 40) # Pemisah antar pengulangan
