7690f9f6eed101e3a03fa081bd2164a08c7fac24
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-15 20:22:50 +0700

    Update gombal.py

--- gombal.py
+++ gombal.py
@@ -9,14 +9,14 @@ def tampilkan_lirik(lirik_dengan_waktu):
 # Daftar lirik lagu dengan waktu per baris (dalam detik)
 lirik_dengan_waktu = [
 
-    ("apa sudah ada kabar lain yang kau tunggu", 5),
-    ("sudah adakah yang gantikan ku", 5),
-    ("yang khawatirkan mu setiap waktu", 5),
-    ("yang cerita tentang apapun sampai hal-hal  tak perlu", 6),
-    ("kalau bisa, jangan buru buru", 5),
-    ("kalau bisa, jangan ada dulu", 5),
+    ("kiniku sadar", 3),
+    ("cintaku ini", 3),
+    ("tak berarti untukmu", 7),
+    ("kau buat luka isi hatiku", 6),
+    ("kau buatku sekecewa itu", 6),
+    ("dimana letak hatimu yang dulu", 7),
+    ("kau duakanku kau memilih dia", 6), 
 ]
 
 # Menjalankan fungsi untuk menampilkan lirik
 tampilkan_lirik(lirik_dengan_waktu)
-
