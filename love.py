44398b68a6bdcb2b913624dd7d1ed2183a2e743d
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-17 10:43:13 +0700

    Update love.py

--- love.py
+++ love.py
@@ -3,19 +3,20 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    kehidupan ini bukan tentang hubungan kamu dan dia,
-    tetapi tentang hubungan seorang hamba kepada tuhannya.
+    Setan tidak menang ketika kamu berbuat dosa,
+    tapi setan meraih kemenangan ketika kamu berfikir 
+    bahwa Allah tidak akan mengampuni.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
-        time.sleep(0.10) # Penundaan 0.10 detik per huruf
+        time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-            tampilkan_kalimat_kata_per_huruf()
-            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-            print('\n' + '-' * 40) # Pemisah antar pengulangan
+        tampilkan_kalimat_kata_per_huruf()
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+        print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
 main()
