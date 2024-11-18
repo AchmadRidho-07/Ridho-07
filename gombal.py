dadd857ea817053a3307d451d00ddc59c23691ab
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-18 18:57:26 +0700

    Update gombal.py

--- gombal.py
+++ gombal.py
@@ -4,18 +4,15 @@ import time
 def tampilkan_lirik(lirik_dengan_waktu):
     for baris, waktu in lirik_dengan_waktu:
         print(baris)
-        time.sleep(waktu)  # Menunggu sesuai detik tertentu
+        time.sleep(waktu) # Menunggu sesuai detik tertentu
 
 # Daftar lirik lagu dengan waktu per baris (dalam detik)
 lirik_dengan_waktu = [
 
-    ("kiniku sadar", 3),
-    ("cintaku ini", 3),
-    ("tak berarti untukmu", 7),
-    ("kau buat luka isi hatiku", 6),
-    ("kau buatku sekecewa itu", 6),
-    ("dimana letak hatimu yang dulu", 7),
-    ("kau duakanku kau memilih dia", 6), 
+    ("Meski beribu bintang terangi hati", 4),
+    ("Hanya kamu yang selalu ku nanti", 4),
+    ("Tanpa dirimu tiada berarti", 5),
+    ("Kau takkan terganti", 1),
 ]
 
 # Menjalankan fungsi untuk menampilkan lirik
