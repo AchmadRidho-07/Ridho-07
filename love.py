9b44faca175808cdc3c1061e27acd2b29af68c2d
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-21 20:12:15 +0700

    deleted

--- love.py
+++ love.py
@@ -3,22 +3,20 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Kemanapun alur cerita hidupmu, tetaplah bahagia
-    menjadi manusia, tetaplah hidup sebagaimana mestinya
-    tetaplah tertawa disela gaduhnya isi kepala,
-    tetaplah menjadi diri sendiri disaat banyaknya
-    pilihan yang membuatmu harus menjadi orang lain.
+    Manusia itu memang unik, ada yang berteduh karena
+    takut jatuh sakit, ada juga yang mencari hujan 
+    untuk melepas rasa sakit.
     """
     for huruf in kalimat:
-        print(huruf, end='', flush=True)
+        print(huruf, end= '', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
-    jumlah_ulang = 1 # set jumlah pengulangan kalimat
+    jumlah_ulang = 1 # set pengulangan kalimat 
     for i in range(jumlah_ulang):
-            tampilkan_kalimat_kata_per_huruf()
-            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-            print('\n' + '-' * 40) # Pemisah antar pengulangan
+        tampilkan_kalimat_kata_per_huruf()
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+        print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
 main()
