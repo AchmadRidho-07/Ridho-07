91672c10560be8afbbd7295364ac95feae09a76e
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-22 19:59:36 +0700

    Update love.py

--- love.py
+++ love.py
@@ -3,20 +3,22 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Manusia itu memang unik, ada yang berteduh karena
-    takut jatuh sakit, ada juga yang mencari hujan 
-    untuk melepas rasa sakit.
+    "Sebenarnya aku pengen banget bahagiain kamu,
+    tapi aku sadar, kelakuanku saja sering bikin kamu kesal."
+
+    "Aku sayang banget sama kamu, tapi maaf yaa
+    kalau aku banyak salahnya"
     """
     for huruf in kalimat:
-        print(huruf, end= '', flush=True)
+        print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
-    jumlah_ulang = 1 # set pengulangan kalimat 
+    jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-        tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-        print('\n' + '-' * 40) # Pemisah antar pengulangan
+            tampilkan_kalimat_kata_per_huruf()
+            time.sleep(1) # PEnundaan 1 detik sebelum pengulangan
+            print('\n' + '-' * 40) # PEmisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
 main()
