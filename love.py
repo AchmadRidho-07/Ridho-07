cfa9559a974354bc4a9a48907cfa42c8c334f474
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-23 09:32:38 +0700

    Update love.py

--- love.py
+++ love.py
@@ -3,11 +3,20 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    "Sebenarnya aku pengen banget bahagiain kamu,
-    tapi aku sadar, kelakuanku saja sering bikin kamu kesal."
+    For : Rizki Dwi Rahmawati
+    Nikmatilah perjalanan hidupmu, meskipun begitu banyak
+    cobaan yang menerpa, karena cobaan itu yang akan
+    menjadikanmu wanita yang tangguh dikemudian hari.
 
-    "Aku sayang banget sama kamu, tapi maaf yaa
-    kalau aku banyak salahnya"
+    Ingatlah setiap langkah yang kamu ambil adalah
+    investasi besar untuk masa depanmu, teruslah melangkah maju
+    karena itu kamu sedang  mendekatkan diri pada cita-citamu.
+
+    capek, lelah, sakit yang kamu rasakan saat ini
+    adalah suatu hal yang wajar, tapi menyerah bukanlah solusinya.
+
+    soo, keep spirit and good luck, wish you all the best
+    in your life.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
@@ -16,9 +25,9 @@ def tampilkan_kalimat_kata_per_huruf():
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-            tampilkan_kalimat_kata_per_huruf()
-            time.sleep(1) # PEnundaan 1 detik sebelum pengulangan
-            print('\n' + '-' * 40) # PEmisah antar pengulangan
+        tampilkan_kalimat_kata_per_huruf()
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+        print('\n' + '-' * 40) # Pemisah antar pengulangan
 
-# Memanggil fungsi main untuk menjalankan program
+# Memanggil fungsi main untuk menjalankan program 
 main()
