ee15e05d06d26f5c35c30e175bbcbe19eb7fa7ea
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-23 20:07:14 +0700

    modified

--- love.py
+++ love.py
@@ -3,21 +3,16 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    For : Rizki Dwi Rahmawati
-    Nikmatilah perjalanan hidupmu, meskipun begitu banyak
-    cobaan yang menerpa, karena cobaan itu yang akan
-    menjadikanmu wanita yang tangguh dikemudian hari.
-
-    Ingatlah setiap langkah yang kamu ambil adalah
-    investasi besar untuk masa depanmu, teruslah melangkah maju
-    karena itu kamu sedang  mendekatkan diri pada cita-citamu.
-
-    capek, lelah, sakit yang kamu rasakan saat ini
-    adalah suatu hal yang wajar, tapi menyerah bukanlah solusinya.
-
-    soo, keep spirit and good luck, wish you all the best
-    in your life.
+    People come and go, itu nyata.
+    hari ini kamu datang, bisa jadi besok 
+    kamu sudah menghilang, namun aku percaya bahwa
+    setiap orang yang tuhan hadirkan dalam kehidupan
+    pasti membawa pelajaran. 
+    
+    sialnya... kamu datang bukan untuk mengajarkan arti cinta sejati,
+    tetapi mengajarkanku arti patah hati.
     """
+
     for huruf in kalimat:
         print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
@@ -26,8 +21,8 @@ def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan 
         print('\n' + '-' * 40) # Pemisah antar pengulangan
 
-# Memanggil fungsi main untuk menjalankan program 
+# Memanggil fungsi main untuk menjalankan program
 main()
