785c10ada94a93621f6ec8eb921f4ed05075e9ae
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-24 22:15:49 +0700

    Update love.py

--- love.py
+++ love.py
@@ -3,14 +3,14 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    People come and go, itu nyata.
-    hari ini kamu datang, bisa jadi besok 
-    kamu sudah menghilang, namun aku percaya bahwa
-    setiap orang yang tuhan hadirkan dalam kehidupan
-    pasti membawa pelajaran. 
-    
-    sialnya... kamu datang bukan untuk mengajarkan arti cinta sejati,
-    tetapi mengajarkanku arti patah hati.
+    Manusia tabiat dasarnya mencintai dirinya,
+    karena itu dia tidak melihat kecuali kebaikan
+    demi kebaikan pada dirinya.
+
+    tetapi... tabiat dasar manusia juga membenci
+    orang yang tidak menyukainya, karena itu 
+    dia tidak melihat kecuali keburukan keburukan
+    yang ada pada orang yang tidak menyukainya.
     """
 
     for huruf in kalimat:
@@ -21,7 +21,7 @@ def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangan 
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
         print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
