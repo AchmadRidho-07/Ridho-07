32fff4125f3029b1d4a886f070c4901122247ef0
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-28 20:42:31 +0700

    MODIFIED

--- love.py
+++ love.py
@@ -3,18 +3,17 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    dari senja aku belajar,
-    seindah apapun cara berpamitan
-    kepergiannya tetap menyisakan kelam
+    Siapa yang mengenal Allah, maka bergaul
+    dengan mahluk sudak tak nikmat lagi.
 
-    sebaik apapun cara kita berpisah
-    kepergianmu tetap menjadi trauma mendalam
+    Siapa yang sudah mengenal dunia,
+    maka dia tidak tertarik lagi mengejarnya.
 
-    setiap perpisahan itu menyakitkan,
-    tidak peduli bagaimana pun caranya berpamitan.
+    Siapa yang sudah mengerti keadilan Allah,
+    maka dia akan menghindari perselisihan 
+    denga manusia.
 
     """
-
     for huruf in kalimat:
         print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
@@ -24,8 +23,7 @@ def main():
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
         time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-        print('\n' + '-' * 40) # Pemisah antar pengulangan
+        print('\n' + '-' * 40) # Pemisah antar pengulangam
 
 # Memanggil fungsi main untuk menjalankan program
 main()
-
