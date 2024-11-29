494fc515ec98ff15e6962a7542c5adc47885e890
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-11-29 17:11:02 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,27 +3,27 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Siapa yang mengenal Allah, maka bergaul
-    dengan mahluk sudak tak nikmat lagi.
+    Dunia itu hanya 3 hari
 
-    Siapa yang sudah mengenal dunia,
-    maka dia tidak tertarik lagi mengejarnya.
+    ~ hari kemarin
+    masa yang tidak dapat diulang
 
-    Siapa yang sudah mengerti keadilan Allah,
-    maka dia akan menghindari perselisihan 
-    denga manusia.
+    ~ hari ini
+    masa untuk beramal
 
+    ~ hari esok
+    masa yang belum pasti
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
-        time.sleep(0.05) # Penundaan 0.05 detik per huruf
+        time.sleep(0.05) # Penundaan 0.05  detik per huruf
 
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-        print('\n' + '-' * 40) # Pemisah antar pengulangam
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangam
+        print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
 main()
