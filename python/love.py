3fdc3ae73c71ad48c92a6b84bf0fef7089e1f3e8
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-01 21:54:05 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -1,28 +1,23 @@
 import time
 
 def tampilkan_kalimat_kata_per_huruf():
-    kalimat = """
+    kalimat= """
 
-    Dunia itu hanya 3 hari
-
-    ~ hari kemarin
-    masa yang tidak dapat diulang
-
-    ~ hari ini
-    masa untuk beramal
-
-    ~ hari esok
-    masa yang belum pasti
+    Dia baik karena memang seharusnya
+    baik ke semua orang, jangan karena baik
+    dikira naksir terus naruh harapan.
+    
+    kalau gitu siapa yang salah?
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
-        time.sleep(0.05) # Penundaan 0.05  detik per huruf
+        time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangam
+        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
         print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
