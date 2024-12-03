d37f97a41a261ea00d1dc7932d38453b2ff2cbaf
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-03 20:53:04 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,13 +3,15 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Berdamailah dengan pikiranmu yang gaduh itu.
-    berbicaralah pada dirimmu saat ini bahwa,
-    tak mengapa bila esok tak baik-baik saja.
+    Di antara senyapnya malam
+    aku kembali mengingatmu.
 
-    Berbanggalah pada dirimu sendiri,
-    yang selama ini sudah mau bertahan dengan
-    kegaduhan pikiran yang sengaja kamu lakukan.
+    kata-kata yang tak sempat kau ucap,
+    rindu yang tak pernah habis,
+    meski sudah terpendam.
+
+    seperti hujan yang tak juga berhenti, 
+    meski tak ada langit yang menangis.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
@@ -19,8 +21,8 @@ def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
             tampilkan_kalimat_kata_per_huruf()
-            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+            time.sleep(1) # penundaan 1 detik sebelum pengulangan k
             print('\n' + '-' * 40) # Pemisah antar pengulangan
 
-# Memanggil fungsi main untuk menjalankan program 
+# Memanggil fungsi main untuk menjalankan program
 main()
