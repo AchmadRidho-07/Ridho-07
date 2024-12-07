541f4bad1a24465383367d260c8147c9c4446b5e
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-07 20:52:38 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,11 +3,10 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Pada akhirnya semua akan berjalan
-    sendiri-sendiri, tidak ada
-    yang membantu mempertanggung
-    jawabkan seluruh perbuatan
-    kecuali diri sendiri.
+    Ketika kamu tidak merasa bahagia
+    dengan hidupmu, maka ketahuilah
+    ada seseorang yang merasa bahagia
+    hanya karena kamu ada.
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
@@ -16,9 +15,9 @@ def tampilkan_kalimat_kata_per_huruf():
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-        tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-        print('\n' + '-' * 40) # Pemisah antar pengulangan
+            tampilkan_kalimat_kata_per_huruf()
+            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+            print('\n' + '-' * 40) # Pemisah antar pengulangan
 
-# Memanggil fungsi main unuk menjalankan program
+# Memanggil fungsi main untuk menjalankan program
 main()
