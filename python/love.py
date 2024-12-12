909e4dadc59db677c076c9e2581c880f77c92d9e
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-12 21:38:27 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,12 +3,15 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Jika kamu mencari yang lebih baik,
-    kamu tidak akan menemukan apa-apa
-    nantinya.
+    Gak penting siapa yang mematahkanmu
+    atau yang membuatmu terluka
 
-    Terkadang yang terbaik tumbuh bersama
-    bukan tersedia begitu saja.
+    yang terpenting adalah siapa yang
+    membuatmu kembali tersenyum.
+
+    ~ Fokuslah pada mereka
+    ~ Bahagiakan mereka
+    ~ Hiduplah dengan mereka
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
@@ -17,9 +20,10 @@ def tampilkan_kalimat_kata_per_huruf():
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-        tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan 1 detik sebelum pengulangan
-        print('\n' + '-' * 40) # Pemisah antar pengulangan
+            tampilkan_kalimat_kata_per_huruf()
+            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+            print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
 main()
+
