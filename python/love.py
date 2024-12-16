3b096e98cb3f84d967944dcd3ba32a920650eb89
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-16 21:39:13 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,9 +3,10 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Seindah apapun kita merencanakan masa depan,
-    tetap sisakan ruang ikhlas bahwa hari esok
-    memang diluar kehendak kita.
+    Manusia itu ada 2
+
+    ~ Manusia yang baik
+    ~ manusia yang berproses menuju baik
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
@@ -14,9 +15,9 @@ def tampilkan_kalimat_kata_per_huruf():
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-        tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan sebelum pengulangan
-        print('\n' + '-' * 40) # Pemisah antar pengulangan
+            tampilkan_kalimat_kata_per_huruf()
+            time.sleep(1) # Penundaan 1 detik sebelum pengulangan
+            print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
 main()
