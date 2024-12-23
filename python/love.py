d0cd8f0ae7d4ba7b4c6df8becfa6317c7abc46a1
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-23 20:39:00 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -1,22 +1,24 @@
 import time
 
-def tampilkan_kalinat_kata_per_huruf():
+def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Kebanyakan manusia ketika berdoa begitu yakin
-    bahwa tuhan maha mendengar, tetapi ketika bermaksiat
-    seakan lupa bahwa tuhan maha melihat.
+    Hidup laki-laki itu isinya hanya tuntunan
+    tanggung jawab dan pertanggung jawaban,
+    lalu tentang salah dan menjadi pihak yang disalahkan,
+    sisanya hanya lamunan dan harapan.
+
     """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
-    jumlah_ulang = 1 # set jumlah pengulangan
+    jumlah_ulang = 1 # set jumlah prngulangan kalimat
     for i in range(jumlah_ulang):
-            tampilkan_kalinat_kata_per_huruf()
+            tampilkan_kalimat_kata_per_huruf()
             time.sleep(1) # Penundaan sebelum pengulangan
             print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
-main() 
+main()
