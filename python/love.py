a57e5f857b4d210cf892e5044cccdd4af1607c6d
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-25 20:13:46 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,22 +3,22 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Hidup laki-laki itu isinya hanya tuntunan
-    tanggung jawab dan pertanggung jawaban,
-    lalu tentang salah dan menjadi pihak yang disalahkan,
-    sisanya hanya lamunan dan harapan.
-
+    Jika hubungan semakin rumit, itu artinya kita
+    dijalan  yang tepat, kita hanya perlu bertahan 
+    dan tetap berjuang, meski dunia  nunjukkin beberapa
+    cara untuk kita berpisah.
     """
+
     for huruf in kalimat:
         print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
-    jumlah_ulang = 1 # set jumlah prngulangan kalimat
+    jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
             tampilkan_kalimat_kata_per_huruf()
             time.sleep(1) # Penundaan sebelum pengulangan
-            print('\n' + '-' * 40) # Pemisah antar pengulangan
+            print('\n' + '-' * 40) # Pemisah antar kalimat
 
 # Memanggil fungsi main untuk menjalankan program
 main()
