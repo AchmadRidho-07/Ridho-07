2ebbb374a4209ad776ab0ec880ec95efa6f5c710
AchmadRidho-07 <ridhoachmad513@gmail.com>
2024-12-27 21:46:38 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,12 +3,13 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Jika hubungan semakin rumit, itu artinya kita
-    dijalan  yang tepat, kita hanya perlu bertahan 
-    dan tetap berjuang, meski dunia  nunjukkin beberapa
-    cara untuk kita berpisah.
+    Rasa sakit itu ketika kamu merindukan seseorang 
+    untuk diajak bicara, kamu ingin tau keberadaanya,
+    kamu ingin menceriktakan hari-harimu kepadanya,
+    dan kemudian kamu mundur karena kamu merasa,
+    dia telah berubah, dan dia tidak mau lagi
+    mendengarkanmu.
     """
-
     for huruf in kalimat:
         print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
@@ -16,9 +17,9 @@ def tampilkan_kalimat_kata_per_huruf():
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-            tampilkan_kalimat_kata_per_huruf()
-            time.sleep(1) # Penundaan sebelum pengulangan
-            print('\n' + '-' * 40) # Pemisah antar kalimat
+        tampilkan_kalimat_kata_per_huruf()
+        time.sleep(1) # Penundaan sebelum pengulangan
+        print('\n' + '-' * 40) # Pemisah antar pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
-main()
+main() 
