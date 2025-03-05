79af6f5376fa36956fbf4e6351147f7d2f16f1aa
AchmadRidho-07 <ridhoachmad513@gmail.com>
2025-03-05 22:45:32 +0700

    commit

--- python/love.py
+++ python/love.py
@@ -3,20 +3,19 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-    Fase paling menyakitkan adalah ketika
-    kamu tidak bisa menjelaskan apa yang kamu rasakan,
-    Berbaring ditempat tidur, mulai dengan tatapan kosong,
-    tetapi kamu bisa merasakan hatimu semakin berat
-    dari detik ke detik.
-    """
+Jangan minta senang, tapi mintalah tenang,
+karena banyak yang terlihat senang tapi
+tidak meraasakan ketenangan.
+"""
     for huruf in kalimat:
         print(huruf, end='', flush=True)
-        time.sleep(0.05) # Penundan 0.05 detik per huruf
+        time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
+        time.sleep(1) # Pengulangan setiap kalimat
         print('\n' + '-' * 40) # Penundaan sebelum pengulangan
 
 # Memanggil fungsi main untuk menjalankan program
