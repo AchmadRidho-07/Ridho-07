36c28fd86d3b56279b26e7c408bc8d872b74cc17
AchmadRidho-07 <ridhoachmad513@gmail.com>
2025-03-14 03:26:19 +0700

    Update love.py

--- python/love.py
+++ python/love.py
@@ -3,20 +3,20 @@ import time
 def tampilkan_kalimat_kata_per_huruf():
     kalimat = """
 
-Jangan minta senang, tapi mintalah tenang,
-karena banyak yang terlihat senang tapi
-tidak meraasakan ketenangan.
-"""
+    Jangan terlalu bergantung pada dunia,
+    karena bayanganmu saja akan meninggalakanmu
+    di saat gelap datang.
+    """
     for huruf in kalimat:
         print(huruf, end='', flush=True)
         time.sleep(0.05) # Penundaan 0.05 detik per huruf
 
 def main():
-    jumlah_ulang = 1 # set jumlah pengulangan kalimat
+    jumlah_ulang = 1 # jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
         tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Pengulangan setiap kalimat
-        print('\n' + '-' * 40) # Penundaan sebelum pengulangan
+        time.sleep(1) # Penundaan sebelum pengulangan
+        print('\n' + '-' * 40) # Pengulangan kalimat
 
 # Memanggil fungsi main untuk menjalankan program
 main()
