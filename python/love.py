9a7502f0ce254631dbf9a6601d262a120287d81a
AchmadRidho-07 <ridhoachmad513@gmail.com>
2025-05-20 21:43:51 +0700

    commit

--- python/love.py
+++ python/love.py
@@ -1,11 +1,12 @@
 import time
 
-def tampilkan_kalimat_kata_per_huruf():
+def tampilkan_kalinat_kata_per_huruf():
     kalimat = """
 
-    Akan datang masanya dimana kamu 
-    dirayakan dan dicintai dengan hebat 
-    oleh seseorang yang bersyukur  memilikimu.
+    Waktu untuk hidup semakin berkurang,
+    jadi jangan sekalipun membiarkan  imanmu
+    ikut berkurang. umur boleh semakin tua dan bertambah
+    tapi tidak dengan dosa.
 
     """
     for huruf in kalimat:
@@ -15,8 +16,8 @@ def tampilkan_kalimat_kata_per_huruf():
 def main():
     jumlah_ulang = 1 # set jumlah pengulangan kalimat
     for i in range(jumlah_ulang):
-        tampilkan_kalimat_kata_per_huruf()
-        time.sleep(1) # Penundaan sebelum pengulangan
+        tampilkan_kalinat_kata_per_huruf()
+        time.sleep(1) # Penndaan sebelum pengulangan
         print('\n' + '-' * 40) # Pengulangan kalimat
 
 # Memanggil fungsi main untuk menjalankan program
