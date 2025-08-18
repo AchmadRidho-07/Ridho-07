f7453c8d24c910fc0a68c5595317c19ac63c3c89
AchmadRidho-07 <ridhoachmad513@gmail.com>
2025-08-18 21:43:38 +0700

    commit

--- dev c++/typeinfo.cpp
+++
@@ -1,9 +0,0 @@
-#include <typeinfo>
-#include <iostream>
-
-int main() {
-    float x = 10;
-    std::cout << "Tipe data dari x: " << typeid(x).name() << std::endl;
-    return 0;
-}
-
--- python/hati.py
+++
@@ -1,6 +0,0 @@
-import time
-#jessa
-count = 1
-while count <= 50000:
-    print('I Love You', count, 'times')
-    count += 1
